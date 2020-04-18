dd335006bfc3a72742c0caaec97e4907ce3d39ef
creativesid <56196602+creativesid@users.noreply.github.com>
2020-04-18 18:57:59 +0530

    update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ class Footer extends React.Component{
     render(){
         return(
             <div className="text-center1">
-                <h6 className="pt-2">Created by Siddharth | data from <a className="ach" href="https://www.covid19india.org/">www.covid19india.org</a></h6>
+                <h6 className="pt-2">Created by <a href="creativesid.github.io">Siddharth</a> | data from <a className="ach" href="https://www.covid19india.org/">www.covid19india.org</a></h6>
             </div>
         )
     }
